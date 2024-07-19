636cf5fbebef6b3a1cb17fefcf76b2d00377606c
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2024-07-19 23:06:35 +0700

    add type for card

--- services/crawler/card_db/card_helper.go
+++ services/crawler/card_db/card_helper.go
@@ -4,12 +4,6 @@ import (
 	utils "go-rec-sys/libs/utils/class"
 )
 
-// Turn raw form of card data into a card object
-
-type CardHelper struct {
-}
-
-func (ch *CardHelper) TransformHTMLToCard(html string) (*utils.Card, error) {
-	// Use utils package to transform HTML string into card object
-	return nil, nil
+func GetCardInfo(cardName string) utils.NormalizedCard {
+	return utils.NormalizedCard{}
 }
